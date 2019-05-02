9ef6a00015f6869a620e6c3aff9aa1e8f95ef49e
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-02 08:28:25 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
+ num1/num2
  
 end
 
 def assign_variable(value)
+  value=value
+end
+
+def argue(arg)
+  return arg
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(arg1,arg2)
 
 end
 
 def return_a_value
-
+  phrase="Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(n="cheese")
+  return  n
 end
